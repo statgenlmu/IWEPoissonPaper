be96f729ff3eae2468d13bdf0fbac2be3e36ca76
Konrad Grosser <grosser@bio.lmu.de>
2018-11-28 11:43:56 +0100

    added copyright by Konrad Grosser and Dirk Metzler

--- src/MCMC/felsenstein_pruning.cc
+++ src/MCMC/felsenstein_pruning.cc
@@ -1,17 +1,19 @@
-   /* This file is part of IWEinference.
 
-    IWEinference is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    IWEinference is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with IWEinference.  If not, see <https://www.gnu.org/licenses/>. */
+/* Copyright (C) 2018 Konrad Grosser and Dirk Metzler */
+/* This file is part of IWEinference.
+   
+   IWEinference is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   
+   IWEinference is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+    
+   You should have received a copy of the GNU General Public License
+   along with IWEinference.  If not, see <https://www.gnu.org/licenses/>. */
 
 #include <cmath>
 #include <vector>
